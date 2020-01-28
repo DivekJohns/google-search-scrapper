9365b19c4a1c1249a2644cb3777f5ca05c42809e
Divek <divekjohn@gmail.com>
2020-01-28 11:57:48 +0530

    feat(added results in json and quick description)

--- app/controller/scrap-controller.js
+++ app/controller/scrap-controller.js
@@ -6,7 +6,7 @@ const json2html = require('html2json').json2html;
 
 
 
-module.exports = class ScrapController {
+module.exports = class ScrapController {	
 	static async getInfo(abtData,count) {
 		return new Promise((resolve, reject) => {
 			let url 
@@ -24,7 +24,12 @@ module.exports = class ScrapController {
 					 search = json2html(search);
 					$ = cheerio.load(html);
 					  searchRes = $(`a`);
-					resolve(html2json(searchRes.toString()).child.filter(e=>(e.attr.onmousedown && !e.attr.href.includes('google'))));
+				
+					  let final = html2json(searchRes.toString()).child.filter(e=>(e.attr.onmousedown && !e.attr.href.includes('google')))
+					  final = final.map(eachRes =>{
+						  return eachRes.attr.href
+					  })
+					resolve({results:final,about: $('[role="heading"]>span>span').text()}); 
 				})
 				.catch(reject); 
 		});
